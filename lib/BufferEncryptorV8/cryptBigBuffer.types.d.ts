76ab30876a89b58a3d5097b80c82b6ad9cb685f9
Rishav Bhowmik <bhowmikrishav@gmail.com>
2021-06-05 12:46:52 +0530

    compilation

--- lib/BufferEncryptorV8/cryptBigBuffer.types.d.ts
+++ lib/BufferEncryptorV8/cryptBigBuffer.types.d.ts
@@ -1,3 +1,16 @@
 /// <reference types="node" />
+/**
+ * ### BigBuffer encrypter with v8 serializer
+ * @param publicKey Public Key as string
+ * @param modLength Maximum length of each chunk encrypted individually
+ * @param data Data to encrypt
+ * @returns Encrypted data
+ */
 export declare type EncryptBigBuffer = (publicKey: string, modLength: number, data: Buffer) => Buffer;
+/**
+ * ### BigBuffer encrypter for v8 serializer data
+ * @param privateKey Private Key as string
+ * @param data Encrypted data to decrypt
+ * @returns Decrypted data
+ */
 export declare type DecryptBigBuffer = (privateKey: string, data: Buffer) => Buffer;
